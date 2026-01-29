b60ad956cf9dd4e4361c9c2943130c403236b30c
paula-saenz-egibide <paula.saenzdeargandona.41690@ikasle.egibide.org>
2026-01-29 20:47:19 +0100

    User control para lista de usuarios

---
+++ Biblioteca/TarjetaUsuario.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Biblioteca
+{
+    partial class TarjetaUsuario
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.userPhoto = new System.Windows.Forms.PictureBox();
+            this.lName = new System.Windows.Forms.Label();
+            this.bDelete = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.userPhoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.userPhoto, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lName, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.bDelete, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(654, 150);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // userPhoto
+            // 
+            this.userPhoto.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.userPhoto.Image = global::Biblioteca.Properties.Resources.usuario;
+            this.userPhoto.Location = new System.Drawing.Point(40, 40);
+            this.userPhoto.Margin = new System.Windows.Forms.Padding(40);
+            this.userPhoto.Name = "userPhoto";
+            this.userPhoto.Size = new System.Drawing.Size(138, 70);
+            this.userPhoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.userPhoto.TabIndex = 0;
+            this.userPhoto.TabStop = false;
+            // 
+            // lName
+            // 
+            this.lName.AutoSize = true;
+            this.lName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lName.Location = new System.Drawing.Point(221, 0);
+            this.lName.Name = "lName";
+            this.lName.Size = new System.Drawing.Size(212, 150);
+            this.lName.TabIndex = 1;
+            this.lName.Text = "Nombre y Apellidos";
+            this.lName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bDelete
+            // 
+            this.bDelete.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bDelete.Location = new System.Drawing.Point(466, 50);
+            this.bDelete.Margin = new System.Windows.Forms.Padding(30, 50, 30, 50);
+            this.bDelete.Name = "bDelete";
+            this.bDelete.Size = new System.Drawing.Size(158, 50);
+            this.bDelete.TabIndex = 2;
+            this.bDelete.Text = "Borrar";
+            this.bDelete.UseVisualStyleBackColor = true;
+            // 
+            // TarjetaUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "TarjetaUsuario";
+            this.Size = new System.Drawing.Size(654, 150);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.userPhoto)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox userPhoto;
+        private System.Windows.Forms.Label lName;
+        private System.Windows.Forms.Button bDelete;
+    }
+}
