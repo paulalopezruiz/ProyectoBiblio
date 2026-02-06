0bf8cd0d69d14ae16587ce5174c5c192adafcdce
paula-saenz-egibide <paula.saenzdeargandona.41690@ikasle.egibide.org>
2026-02-06 19:43:32 +0100

    tarejta libros creada

---
+++ Biblioteca/TarjetaLibro.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Biblioteca
+{
+    partial class TarjetaLibro
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.bBorrar = new System.Windows.Forms.Button();
+            this.pbPortada = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPortada)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.pbPortada, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.bBorrar, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 63.73626F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 36.26374F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(257, 273);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // bBorrar
+            // 
+            this.bBorrar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bBorrar.Location = new System.Drawing.Point(25, 193);
+            this.bBorrar.Margin = new System.Windows.Forms.Padding(25, 20, 25, 20);
+            this.bBorrar.Name = "bBorrar";
+            this.bBorrar.Size = new System.Drawing.Size(207, 60);
+            this.bBorrar.TabIndex = 1;
+            this.bBorrar.Text = "Borrar";
+            this.bBorrar.UseVisualStyleBackColor = true;
+            // 
+            // pbPortada
+            // 
+            this.pbPortada.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbPortada.Location = new System.Drawing.Point(3, 3);
+            this.pbPortada.Name = "pbPortada";
+            this.pbPortada.Size = new System.Drawing.Size(251, 167);
+            this.pbPortada.TabIndex = 0;
+            this.pbPortada.TabStop = false;
+            this.pbPortada.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // TarjetaLibro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "TarjetaLibro";
+            this.Size = new System.Drawing.Size(257, 273);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPortada)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pbPortada;
+        private System.Windows.Forms.Button bBorrar;
+    }
+}
